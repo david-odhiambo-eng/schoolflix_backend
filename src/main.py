0d334ec0a40503853a7c537229ad5dc15b229cc8
eng-david-odhiambo <odhiambodavid305@gmail.com>
2025-09-29 11:07:45 +0300

    update the ruff setup and precommmit

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
+
+import os
+
+from pathlib import Path
+
 app = FastAPI(docs_url='/', title='schoolflix', version='v1')
 
 
 @app.get('/health')
 async def health():
-	return {
-
-
-
-	}
+	return {}
